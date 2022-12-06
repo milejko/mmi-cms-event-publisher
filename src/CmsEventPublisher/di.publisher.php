87b8b0139d79f41cc54c4a40b5236a0f799f1f73
Wojciech M <wojciech.miedzybrodzki@gmail.com>
2022-12-06 10:43:33 +0100

    feat: vhost support in amqp publisher (#1)
    Co-authored-by: Wojciech Miedzybrodzki <w.miedzybrodzki@nowaera.pl>

--- src/CmsEventPublisher/di.publisher.php
+++ src/CmsEventPublisher/di.publisher.php
@@ -11,6 +11,7 @@ return [
     'cmsEventPublisher.rabbitmq.enabled'  => env('CMS_EVENT_PUBLISHER_RABBITMQ_ENABLED', false),
     'cmsEventPublisher.rabbitmq.host'     => env('CMS_EVENT_PUBLISHER_RABBITMQ_HOST', 'localhost'),
     'cmsEventPublisher.rabbitmq.port'     => env('CMS_EVENT_PUBLISHER_RABBITMQ_PORT', 5672),
+    'cmsEventPublisher.rabbitmq.vhost'    => env('CMS_EVENT_PUBLISHER_RABBITMQ_VHOST', 'cms'),
     'cmsEventPublisher.rabbitmq.username' => env('CMS_EVENT_PUBLISHER_RABBITMQ_USERNAME', ''),
     'cmsEventPublisher.rabbitmq.password' => env('CMS_EVENT_PUBLISHER_RABBITMQ_PASSWORD', ''),
 
@@ -25,7 +26,8 @@ return [
             $container->get('cmsEventPublisher.rabbitmq.host'),
             $container->get('cmsEventPublisher.rabbitmq.port'),
             $container->get('cmsEventPublisher.rabbitmq.username'),
-            $container->get('cmsEventPublisher.rabbitmq.password')
+            $container->get('cmsEventPublisher.rabbitmq.password'),
+            $container->get('cmsEventPublisher.rabbitmq.vhost'),
         );
     }
 ];
