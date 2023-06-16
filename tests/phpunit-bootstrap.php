dcc748866b9d287d8c4c04c3130872481c8af990
Mariusz Milejko <mariusz@milejko.pl>
2023-06-16 16:44:03 +0200

    session path fix

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -7,5 +7,8 @@ define('BASE_PATH', realpath(__DIR__ . '/../'));
 //dołączenie autoloadera
 require BASE_PATH . '/vendor/autoload.php';
 
+putenv('APP_DEBUG_ENABLED=0');
+putenv('SESSION_PATH=/tmp');
+
 //run application
 (new AppTesting())->run();
