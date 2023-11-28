a5c5e31e5731debec52b080f436d817ce1310c64
Mariusz Milejko <mariusz@milejko.pl>
2023-11-28 21:04:57 +0100

    more conservative publisher

--- src/CmsEventPublisher/AmqpMessagePublisher.php
+++ src/CmsEventPublisher/AmqpMessagePublisher.php
@@ -12,6 +12,12 @@ class AmqpMessagePublisher implements MessagePublisherInterface
     private const EXCHANGE_PASSIVE = false;
     private const EXCHANGE_AUTODELETE = false;
 
+    private const CONNECTION_TIMEOUT = 1.0;
+    private const READ_WRITE_TIMEOUT = 2.0;
+
+    private AMQPChannel $channel;
+    private AMQPStreamConnection $connection;
+
     public function __construct(
         private string $host,
         private int $port,
@@ -24,20 +30,50 @@ class AmqpMessagePublisher implements MessagePublisherInterface
 
     public function publish(MessageInterface $message): void
     {
-        $connection = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password, $this->vhost);
-        $channel = $connection->channel();
+        $this->getChannel()->basic_publish(new AMQPMessage($message->getContent()), $this->exchange, $message->getRoute());
+    }
+
+    private function getConnection(): AMQPStreamConnection
+    {
+        if ($this->connection instanceof AMQPStreamConnection) {
+            return $this->connection;
+        }
+        return $this->connection = new AMQPStreamConnection(
+            $this->host,
+            $this->port,
+            $this->user,
+            $this->password,
+            $this->vhost,
+            false,
+            'AMQPLAIN',
+            null,
+            'en_US',
+            self::CONNECTION_TIMEOUT,
+            self::READ_WRITE_TIMEOUT,
+        );
+    }
+
+    private function getChannel(): AMQPChannel
+    {
+        if ($this->channel instanceof AMQPChannel) {
+            return $this->channel;
+        }
+        $this->channel = $this->getConnection()->channel();
 
         //create the exchange if it doesn't exist already
-        $channel->exchange_declare(
+        $this->channel->exchange_declare(
             $this->exchange,
             self::EXCHANGE_TYPE,
             self::EXCHANGE_PASSIVE,
             self::EXCHANGE_DURABLE,
             self::EXCHANGE_AUTODELETE
         );
+        return $this->channel;
+    }
 
-        $channel->basic_publish(new AMQPMessage($message->getContent()), $this->exchange, $message->getRoute());
-        $channel->close();
-        $connection->close();
+    public function __destruct()
+    {
+        $this->channel->close();
+        $this->connection->close();
     }
 }
